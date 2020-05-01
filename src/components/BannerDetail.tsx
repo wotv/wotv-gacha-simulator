1ce50e13e148c1db4ac790d926856e30d7a6753d
wotv <>
2020-05-02 05:00:02 +0900

    소환 버튼 추가

--- src/components/BannerDetail.tsx
+++ src/components/BannerDetail.tsx
@@ -1,12 +1,21 @@
-import {Collapse, Layout, PageHeader} from "antd";
+import {Badge, Button, Checkbox, Collapse, Layout, PageHeader, Space, Popover, Input} from "antd";
 import React from "react";
 import {Banner} from "../data/banners";
+import {sort} from "../util";
+import {Unit} from "../data/units";
+import PopoverButton from "./PopoverButton";
 
 interface BannerDetailProps {
     banner: Banner;
 }
 
-const {Panel} = Collapse;
+const sortUnitsByRank = (units: Unit[]) => {
+    return sort(
+        units,
+        "rank",
+        ["UR", "MR", "SR", "R", "N"]
+    );
+};
 
 const BannerDetail: React.FC<BannerDetailProps & React.HTMLAttributes<HTMLElement>> = ({banner, ...attributes}) => (
     <Layout {...attributes}>
@@ -17,14 +26,38 @@ const BannerDetail: React.FC<BannerDetailProps & React.HTMLAttributes<HTMLElemen
                  style={{width: "100%"}}/>
         </div>
         <>
-            <Collapse bordered={false} defaultActiveKey={["1"]}>
-                <Panel header="출현 유닛" key="1">
-                    {banner.units.map((unit, index) => (
+            <Collapse bordered={false} defaultActiveKey={"1"} style={{overflowY: "scroll"}}>
+                <Collapse.Panel header="소환" key="1">
+                    <Space direction="horizontal" size={12}>
+                        <Button type="primary" size="middle">1회 소환</Button>
+                        <PopoverButton button={<Button type="primary">1 * N회 소환</Button>}>
+                            {
+                                (close: Function) => <Space direction={"horizontal"}>
+                                    <Input type="number" defaultValue={1} placeholder="횟수 입력: " style={{width: "80px"}}/>
+                                    <Button type="primary" onClick={() => close()}>반복 소환</Button>
+                                </Space>
+                            }
+                        </PopoverButton>
+
+                        <Button type="primary" size="middle">10회 소환</Button>
+                        <PopoverButton button={<Button type="primary">10 * N회 소환</Button>}>
+                            {
+                                (close: Function) => <Space direction={"horizontal"}>
+                                    <Input type="number" defaultValue={1} placeholder="횟수 입력: " style={{width: "80px"}}/>
+                                    <Button type="primary" onClick={() => close()}>반복 소환</Button>
+                                </Space>
+                            }
+                        </PopoverButton>
+                    </Space>
+                </Collapse.Panel>
+                <Collapse.Panel header="출현 유닛" key="2">
+                    {sortUnitsByRank(banner.units).map((unit, index) => (
                         <div key={index}>
                             {unit.name} ({unit.rank.toString()})
+                            {banner.pickUpUnits.includes(unit) ? <span style={{color: "green"}}> [픽업]</span> : ""}
                         </div>
                     ))}
-                </Panel>
+                </Collapse.Panel>
             </Collapse>
         </>
     </Layout>
