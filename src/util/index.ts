47fda6203c5bf2013b20c27636633aad64f7df96
wotv <>
2020-05-03 08:54:50 +0900

    소환 배너 이미지, 비전카드 이미지 추가

--- src/util/index.ts
+++
@@ -1,7 +0,0 @@
-import _ from "lodash";
-
-export const sort = <T>(list: T[], column: keyof T, orders: string[]) => {
-    return _.sortBy(list, (item) =>
-        _.indexOf(orders, item[column] as any)
-    )
-};
