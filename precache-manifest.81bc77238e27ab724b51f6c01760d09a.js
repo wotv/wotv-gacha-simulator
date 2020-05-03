bac1bb9660eebea3e76c34b4ec83293b5c5833c0
wotv <>
2020-05-03 09:34:22 +0900

    Updates

---
+++ precache-manifest.81bc77238e27ab724b51f6c01760d09a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4cb91cb5fc249a6231091b976af134",
+    "url": "/wotv-gacha-simulator/index.html"
+  },
+  {
+    "revision": "f515e740539d2ab5fca1",
+    "url": "/wotv-gacha-simulator/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "c243a3595fc6f27791dc",
+    "url": "/wotv-gacha-simulator/static/css/main.72323b8c.chunk.css"
+  },
+  {
+    "revision": "f515e740539d2ab5fca1",
+    "url": "/wotv-gacha-simulator/static/js/2.299d9ec3.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/wotv-gacha-simulator/static/js/2.299d9ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c243a3595fc6f27791dc",
+    "url": "/wotv-gacha-simulator/static/js/main.6e69527a.chunk.js"
+  },
+  {
+    "revision": "d74e2d229ec69b6dae10",
+    "url": "/wotv-gacha-simulator/static/js/runtime-main.c17f3697.js"
+  }
+]);
