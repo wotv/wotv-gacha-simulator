df189d6a3cfd0c0de7ede87e27be9c2e02d086bd
wotv <>
2020-05-03 09:01:20 +0900

    banner action, modal reducer 추가

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,22 @@
-import {
-    configureStore,
-    createSlice,
-    PayloadAction
-} from "@reduxjs/toolkit";
+import {configureStore, createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {Banner} from "./data/banners";
+import {Unit} from "./data/units";
+
+type SummonInfo = {
+    unitCount: number;
+    times: number;
+}
 
 const bannerSlice = createSlice({
     name: "banner",
     initialState: {
         banners: [],
-        selectedBanner: null
+        selectedBanner: null,
+        summonInfo: {
+            unitCount: 1,
+            times: 1,
+            pulledUnits: [] as Unit[]
+        },
     },
     reducers: {
         loadBanners: (state: any, action: PayloadAction<Banner[]>) => {
@@ -17,18 +24,50 @@ const bannerSlice = createSlice({
         },
         setSelectedBanner: (state: any, action: PayloadAction<Banner | null>) => {
             state.selectedBanner = action.payload
+        },
+        setSummonInfo: (state: any, action: PayloadAction<SummonInfo>) => {
+            state.summonInfo.unitCount = action.payload.unitCount;
+            state.summonInfo.times = action.payload.times;
+        },
+        addPulledUnits: (state: any, action: PayloadAction<Unit[]>) => {
+            state.summonInfo.pulledUnits = [
+                ...state.summonInfo.pulledUnits,
+                ...action.payload
+            ]
+        },
+        clearPulledUnits: (state: any) => {
+            state.summonInfo.pulledUnits = [];
         }
     }
 });
 
-export const {loadBanners, setSelectedBanner} = bannerSlice.actions;
+export const {loadBanners, setSelectedBanner, setSummonInfo, addPulledUnits, clearPulledUnits} = bannerSlice.actions;
 
 export const selectBanners = (state: RootState) => state.banner.banners;
 export const selectSelectedBanner = (state: RootState) => state.banner.selectedBanner;
+export const selectSummonInfo = (state: RootState) => state.banner.summonInfo;
+export const selectPulledUnits = (state: RootState) => state.banner.summonInfo.pulledUnits;
+
+const modalSlice = createSlice({
+    name: "modal",
+    initialState: {
+        visible: false,
+    },
+    reducers: {
+        setVisible: (state: any, action: PayloadAction<boolean>) => {
+            state.visible = action.payload;
+        }
+    }
+});
+
+export const {setVisible} = modalSlice.actions;
+
+export const selectModalVisible = (state: RootState) => state.modal.visible;
 
 const store = configureStore({
     reducer: {
-        banner: bannerSlice.reducer
+        banner: bannerSlice.reducer,
+        modal: modalSlice.reducer,
     }
 });
 
